8e4e19cc984755ea88ff5563aec83b6492ad784f
ffozdemir <60287755+ffozdemir@users.noreply.github.com>
2022-02-21 15:17:02 +0300

    hata yönetimi first

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine("Bir sayı giriniz");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayi :" + sayi1);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata:" + ex.Message.ToString());
+            }
+
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }   
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-200000000000");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil.");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük veya çok büyük değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem başarıyla tamamlandı.");
+            }
+            
+        }
+    }
+}
